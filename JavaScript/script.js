108ade049ab50d5e67c9cd1480cfa1d4d186e487
Andrei11BC <adiaconiteiandrei113@gmail.com>
2024-05-14 09:38:12 +0300

    API functional

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,45 +1,18 @@
-const newsSection = document.getElementById('news');
-
-function createNewsButton(article) {
-    const button = document.createElement('button');
-    button.textContent = article.title;
-    button.addEventListener('click', () => {
-        window.open(article.url, '_blank');
-    });
-    return button;
-}
-
-function displayNewsButtons(articles) {
-    articles.forEach(article => {
-        const button = createNewsButton(article);
-        newsSection.appendChild(button);
-    });
-}
-
 const apiKey = '3fc98b634c8b4a178a9d0128d82cb953';
-const apiUrl = `https://newsapi.org/v2/everything?q=tesla&from=2024-04-13&sortBy=publishedAt&apiKey=${apiKey}`;
+const sortOrder = 'published'
+const date = '2024-05-13'
+const query = 'tesla'
 
-fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-        const articles = data.articles;
-        displayNewsButtons(articles);
-    })
-    .catch(error => console.log('Error fetching news:', error));
+const fetchNews = () =>
+  fetch(`https://newsapi.org/v2/everything?q=${query}&from=${date}&sortBy=${sortOrder}&apiKey=${apiKey}`);
 
-function openArticle(url) {
-    window.open(url, '_blank');
-}
+const getNews = async (count) => {
+    const deck = await fetchNews();
+    const data = await deck.json(); 
+    return data;
+};
 
-// Atașăm evenimente de click butoanelor (poți adapta aceasta parte la nevoile tale)
-document.getElementById('button1').addEventListener('click', function() {
-    openArticle(apiUrl); // Deschide URL-ul API-ului
-});
-
-document.getElementById('button2').addEventListener('click', function() {
-    openArticle(apiUrl); // Deschide URL-ul API-ului
-});
-
-document.getElementById('button3').addEventListener('click', function() {
-    openArticle(apiUrl); // Deschide URL-ul API-ului
+// Get Deck Id
+getNews().then(data => {
+  console.log(data); 
 });
